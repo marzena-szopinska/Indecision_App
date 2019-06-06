c3695a391b3820e365afee705311bcace58702c6
Marzena <marzena.szopinska090@gmail.com>
2019-06-06 13:57:47 +0200

    working on indecision app (part one)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {/* render paragraph only if subtitle exists */}
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {app.options.length > 0 ? 'Here are your options' : 'No options to show'}
-        <ol>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    // prevent page reload on submit
+    e.preventDefault();
+    // if there is an option, store it in a variable
+    const option = e.target.elements.option.value;
 
-// practicing
+    if(option){
+        app.options.push(option);
+        // clean the input window
+        e.target.elements.option.value = '';
+        // rerender the app
+        renderApp();
+    }
+};
 
-// var templateTwo = (
-//     <div>
-//         <h1>Marzena Szopinska</h1>
-//         <p>Age: 29</p>
-//         <p>Location: Poland</p>
-//     </div>
-// );
+const onRemoveAll = () => {
+    // empty array
+    app.options = [];
+    // rerender the app
+    renderApp();
+};
 
-// render the app
-ReactDOM.render(template, document.getElementById('root'));
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {/* render paragraph only if subtitle exists */}
+            {app.subtitle && <p>{app.subtitle}</p>}
+            {app.options.length > 0 ? 'Here are your options' : 'No options to show'}
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    
+    
+    // render the app
+    ReactDOM.render(template, document.getElementById('root'));
+};
+
+renderApp();
+
+
+
