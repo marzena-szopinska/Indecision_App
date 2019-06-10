79e6264a5b80e4dd212055ed89ecc8566bec790f
Marzena <marzena.szopinska090@gmail.com>
2019-06-10 12:55:49 +0200

    working on my class based components

--- src/index.js
+++ src/index.js
@@ -1,68 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    // prevent page reload on submit
-    e.preventDefault();
-    // if there is an option, store it in a variable
-    const option = e.target.elements.option.value;
-
-    if(option){
-        app.options.push(option);
-        // clean the input window
-        e.target.elements.option.value = '';
-        // rerender the app
-        renderApp();
+class IndecisionApp extends React.Component {
+    render(){
+        const title = 'Indecision App';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['one', 'two', 'three'];
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle}/>
+                <Action />
+                <Options options={options}/>
+                <AddOption />
+            </div>
+        );
     }
-};
+}
 
-const onRemoveAll = () => {
-    // empty array
-    app.options = [];
-    // rerender the app
-    renderApp();
-};
-
-const onMakeDecision = () => {
-    const randNumber = Math.floor(Math.random() * app.options.length);
-    const pickedOption = app.options[randNumber];
-    alert(pickedOption);
-};
-
-const renderApp = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {/* render paragraph only if subtitle exists */}
-            {app.subtitle && <p>{app.subtitle}</p>}
-            {app.options.length > 0 ? 'Here are your options' : 'No options to show'}
-            <p>{app.options.length}</p>
-            <button disabled={app.options.length == 0} onClick={onMakeDecision}>What Should I Do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                { /* render the options list */
-                    app.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type='text' name='option' />
-                <button>Add Option</button>
-            </form>
-        </div>
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
     );
-    
-    
-    // render the app
-    ReactDOM.render(template, document.getElementById('root'));
-};
+  }
+}
 
-renderApp();
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What Should I Do?</button>
+      </div>
+    );
+  }
+}
+
+// Render new p tag for each option, set the key and the value
+class Options extends React.Component {
+  render(){
+    return (
+      <div>
+        {
+            this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.optionText}</p>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+  render(){
+    return (
+      <div>
+        <p>AddOption component here!</p>
+      </div>
+    );
+  }
+}
 
 
-
+ReactDOM.render(<IndecisionApp />, document.getElementById('root'));
---
+++ src/playground.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    // prevent page reload on submit
+    e.preventDefault();
+    // if there is an option, store it in a variable
+    const option = e.target.elements.option.value;
+
+    if(option){
+        app.options.push(option);
+        // clean the input window
+        e.target.elements.option.value = '';
+        // rerender the app
+        renderApp();
+    }
+};
+
+const onRemoveAll = () => {
+    // empty array
+    app.options = [];
+    // rerender the app
+    renderApp();
+};
+
+const onMakeDecision = () => {
+    const randNumber = Math.floor(Math.random() * app.options.length);
+    const pickedOption = app.options[randNumber];
+    alert(pickedOption);
+};
+
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {/* render paragraph only if subtitle exists */}
+            {app.subtitle && <p>{app.subtitle}</p>}
+            {app.options.length > 0 ? 'Here are your options' : 'No options to show'}
+            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I Do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                { /* render the options list */
+                    app.options.map((option) => <li key={option}>{option}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    
+    
+    // render the app
+    //ReactDOM.render(template, document.getElementById('root'));
+};
+
+//renderApp();
